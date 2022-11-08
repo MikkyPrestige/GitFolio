21a3082e2fcb906d25637b0a5799466840b99e83
Mikky <meekyberry6@gmail.com>
2022-11-08 06:56:08 +0100

    Code update

---
+++ .history/src/components/layout_20221108063637.jsx
@@ -0,0 +1,88 @@
+// LAYOUT COMPONENT - This component is the layout for the Navigation bar. It contains the header and the Navigation for the app.
+
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../assets/styles/layout.css";
+import Logo from "../assets/img/rest-api-logo.webp";
+import { useState } from "react";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
+
+const CustomNavLink = ({ to, ...props }) => {
+  let activeStyle = {
+    color: "red",
+    fontWeight: "bold",
+    textDecoration: "underline",
+  };
+
+  return (
+    <NavLink
+      to={to}
+      style={({ isActive }) =>
+        isActive ? activeStyle : { textDecoration: "none" }
+      }
+      {...props}
+    />
+  );
+};
+
+const Layout = () => {
+  const [nav, setNav] = useState(false);
+
+  const toggleNav = () => {
+    setNav(!nav);
+  };
+
+  const closeNav = () => {
+    setNav(false);
+  };
+
+  return (
+    <div className="layout">
+      <header className="layout--header">
+        <div className="layout--header--logo">
+          <img
+            src={Logo}
+            alt="logo"
+            style={{ width: "8rem", height: "4rem" }}
+          />
+        </div>
+        <nav className="layout--items">
+          <button onClick={toggleNav} className="layout--btn">
+            {nav ? (
+              <MdClose className="layout--btn--icon" />
+            ) : (
+              <FiMenu className="layout--btn--icon" />
+            )}
+          </button>
+          <div className={`menuNav ${nav ? " showMenu" : ""}`}>
+            <CustomNavLink
+              to="/"
+              end
+              className="layout--item"
+              onClick={() => closeNav()}
+            >
+              Home
+            </CustomNavLink>
+            <CustomNavLink
+              to="repos"
+              className="layout--item"
+              onClick={() => closeNav()}
+            >
+              Repositories
+            </CustomNavLink>
+            <CustomNavLink
+              to="contact"
+              className="layout--item"
+              onClick={() => closeNav()}
+            >
+              Contact
+            </CustomNavLink>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Layout;
